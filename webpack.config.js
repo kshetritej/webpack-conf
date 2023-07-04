951f2fad0767eae15492c08c8b7fff80f5414b76
kshetritej <furiousdtej@gmail.com>
2023-07-04 12:54:07 +0545

    update webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,18 @@
-const path = require('path') // it will allow us to use __dirname and use paths obviously
-const HtmlWebpackPlugin = require('html-webpack-plugin') 
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    mode: 'development', // It creates minified code in bundle[contenthash].js, mode: 'production', can be used for readable outputs
+    mode: 'development',
     entry:{
-        bundle: path.resolve(__dirname, 'src/index.js'), //Webpack will look here for the main .js file
+        bundle: path.resolve(__dirname, 'src/index.js'),
     },
-    output:{ 
-        /* All about outputs path,filename,*/
+    output:{
         path: path.resolve(__dirname,'dist'),
         filename: '[name][contenthash].js',
-        clean: true, // clears the existing file every time 'npm run build' is executed
-        assetModuleFilename : '[name][ext]', // prevents filename from being overwritten by random filename
+        clean: true,
+        assetModuleFilename : '[name][ext]',
     },
-    // devtool: 'source-map', //it will create a new .map.js file in dist which helps with debugging.
-
-    // DevServer Configuration: Opens index.html from dist folder in localhost:3000,
+    // devtool: 'source-map',
     devServer: {
         static: {
             directory : path.resolve(__dirname,'dist'),
@@ -29,7 +26,7 @@ module.exports = {
     module:{
         rules: [
             {
-                test: /\.scss$/,//RegExp -- Look for files that ends with .scss extension (if you want to look for .css files it should be /\.css$/, )
+                test: /\.scss$/,
                 use:[
                     'style-loader',
                     'css-loader',
@@ -47,7 +44,7 @@ module.exports = {
                 },
             },
             {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i, //Asset loader (the 'i' represents 'insensitive' for case-insensitive file names)
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
             }
         ],
@@ -55,9 +52,9 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Webpack App', // Use as title in template.html using <% = HtmlWebpackPlugin.options.title %>
-            filename: 'index.html', //It will be created automatically, when npm run dev is executed
-            template: 'src/template.html', //Template should be defined manually
+            title: 'Webpack App',
+            filename: 'index.html',
+            template: 'src/template.html',
         }),
     ],
 }
